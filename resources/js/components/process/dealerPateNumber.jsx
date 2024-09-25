121f3c3f14bb05bd6dc5f2b34a675e2e636a0ee5
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-09-25 18:26:23 +0100

    commit

--- resources/js/components/process/dealerPateNumber.jsx
+++ resources/js/components/process/dealerPateNumber.jsx
@@ -376,7 +376,9 @@ export default function DealerPateNumber() {
                                                     <div className="col-md-1 mb-2"></div>
                                                     <div className="card-body col-md-10 align-items-center text-center">
                                                         <div id="mainPrice" className="alert alert-info mt-3">
-                                                        Total Amount: ₦<span>{totalAmount.toLocaleString()}</span>
+                                                        Total Amount: 
+                                                        <span  >{new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN', minimumFractionDigits: 2 }).format(totalAmount)}</span>
+                                                               
                                                         </div>
                                                     </div>
                                                     <div className="col-md-1 mb-2"></div>
