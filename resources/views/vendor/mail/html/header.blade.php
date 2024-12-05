3be7828462f7c8d2cd12ed817f6e09807fc2c3d4
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-12-05 16:48:01 +0100

    comment

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel') 
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="{{ asset('assets/img/rab.png')}}" class="logo" alt="Laravel Logo">
 @else 
-<img src="https://rabmotlicensing.com/rabmot.png" class="logo" alt="Rabmot Licensing Agency" width="1200px" height="90px">
+<img src="{{ asset('assets/img/rab.png')}}" class="logo" alt="Rabmot Licensing Agency" width="1200px" height="120px">
 @endif
 </a>
 </td>
