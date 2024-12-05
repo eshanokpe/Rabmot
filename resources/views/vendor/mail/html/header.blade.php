04bc0b3212790b32f0f3b446003113a860d4e089
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-12-05 16:38:13 +0100

    comment

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel') 
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
+@else 
 <img src="https://rabmotlicensing.com/rabmot.png" class="logo" alt="Rabmot Licensing Agency" width="1200px" height="90px">
 @endif
 </a>
