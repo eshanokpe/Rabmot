410543accf8e8dd95c7dd8141e1e5cd2e7b18691
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-12-28 12:16:02 +0100

    comment

--- resources/views/user/pages/checkout.blade.php
+++ resources/views/user/pages/checkout.blade.php
@@ -33,14 +33,23 @@
                 <div class="card radius-5 p-3">
                     <div class="card-body">
                         @if (session('success'))
-                        <div class="col-sm-12">
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                {{ session('success') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>
+                            <div class="col-sm-12">
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    {{ session('success') }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
 
-                        </div>
-                    @endif
+                                    </div>
+                            @endif
+                            @if (session('error'))
+                            <div class="col-sm-12">
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    {{ session('error') }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+
+                            </div>
+                        @endif
                         <div class="row">
                             <div class="col-12 col-sm-8">
                                 <div class="card-title">
