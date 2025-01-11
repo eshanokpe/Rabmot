d4b2f6cd64786a24b8cab5b5d01aaa3f553004e5
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-01-11 02:59:14 +0100

    comment

--- resources/views/user/partials/topnav.blade.php
+++ resources/views/user/partials/topnav.blade.php
@@ -17,7 +17,7 @@
             <ul class="navbar-nav">
                 <li class="nav-item search-btn-mobile">
                     <a class="nav-link position-relative" href="javascript:;">	<i class="bx bx-search vertical-align-middle"></i>
-                    </a>
+                    </a>  
                 </li>
                 <li class="nav-item dropdown dropdown-lg">
                     <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href=" {{ route('home.cart') }}" >	
