afcca2b3992082c74c57cb2efa1750d02357f375
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-02-20 13:11:08 +0100

    comment

--- app/Http/Controllers/Admin/ProcessDocument/AdminProcessDocument.php
+++ app/Http/Controllers/Admin/ProcessDocument/AdminProcessDocument.php
@@ -31,7 +31,7 @@ class AdminProcessDocument extends Controller
          'status' => 'required|in:0,1,2,3,4',
       ]); 
       $user = ProcessHistory::where('id', decrypt($id))->first();
-      
+      dd( $user);
       if($request->input('status') == 1){
          $users = User::where('email',$user->user_email)->get()->first();
          $email = new ProcessingMode($users); 
